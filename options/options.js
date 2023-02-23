927f915fc5d14ea5b1186315bda5308b0de16e62
Crowdbotics <team@crowdbotics.com>
2023-02-23 20:37:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tango-39090",
+  url: "https://tango-39090.botics.co",
+  api: "https://tango-39090.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
